c24fb0d8b014b7272cefa27eeb0f74db4f8c297a
Cyril Muchiri <cyrilmuchiri11@gmail.com>
2026-01-05 11:00:40 +0300

    refactored to use Lombok annotations for constructor injection

--- src/main/java/com/mmsolutions/votifyv2/service/ContestOnboardingService.java
+++ src/main/java/com/mmsolutions/votifyv2/service/ContestOnboardingService.java
@@ -4,18 +4,16 @@ import com.mmsolutions.votifyv2.dto.ContestDto;
 import com.mmsolutions.votifyv2.entity.Contest;
 import com.mmsolutions.votifyv2.mapper.ContestDtoMapper;
 import com.mmsolutions.votifyv2.repository.ContestRepository;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class ContestOnboardingService {
 
     ContestRepository contestRepository;
     ContestDtoMapper contestDtoMapper;
-
-    public ContestOnboardingService(ContestRepository contestRepository, ContestDtoMapper contestDtoMapper) {
-        this.contestRepository = contestRepository;
-        this.contestDtoMapper = contestDtoMapper;
-    }
 
     public String createContest(ContestDto contestDto) {
         Contest contest = contestDtoMapper.mapToContestEntity(contestDto);
--- src/main/java/com/mmsolutions/votifyv2/service/UserCreationService.java
+++ src/main/java/com/mmsolutions/votifyv2/service/UserCreationService.java
@@ -5,21 +5,17 @@ import com.mmsolutions.votifyv2.dto.UserRequestDTO;
 import com.mmsolutions.votifyv2.entity.AppUser;
 import com.mmsolutions.votifyv2.mapper.AppUserMapper;
 import com.mmsolutions.votifyv2.repository.AppUserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class UserCreationService {
 
     AppUserRepository appUserRepository;
     AppUserMapper appUserMapper;
     PasswordEncoder passwordEncoder;
-
-    public UserCreationService(AppUserRepository userRepository, AppUserMapper appUserMapper, PasswordEncoder passwordEncoder) {
-        this.appUserRepository = userRepository;
-        this.appUserMapper = appUserMapper;
-        this.passwordEncoder = passwordEncoder;
-    }
 
     public boolean checkUserExists(AppUser user){
        return appUserRepository.findByEmail(user.getEmail()).isPresent();
